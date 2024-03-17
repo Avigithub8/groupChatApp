7ef1c8709fce96289c9d6b7398605dfb72c43723
Avigithub8 <sharma.avi844@gmail.com>
2024-03-17 22:53:40 +0530

    using cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const db=require('./utils/db.js');
 const User = require('./models/users.js');
+const cors=require("cors");
 const env = require("dotenv");
 env.config()
 
@@ -15,7 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/public',express.static(path.join(__dirname,'public')));
 //app.use(express.static(__dirname,'public'));
-
+app.use(cors({
+    origin:'http://localhost:5000', 
+}))
 
 
 app.use("/user", userRoutes);
@@ -23,8 +26,11 @@ app.use("/user", userRoutes);
 app.get('/', (req, res) => { 
     res.send('A simple Node App is '
         + 'running on this server') 
+    
     res.end() 
 }) 
+
+
  
 
 
@@ -39,4 +45,26 @@ db.sync()
 
 const PORT = 5000;
 app.listen(PORT,console.log(
-  `Server started on port ${PORT}`));
+  `Server started on port ${PORT}`))
+
+
+////////////////////////////////////////////////
+
+
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(cors({
+//     origin:'http://127.0.0.1:5000', 
+// }))
+
+// app.get('/api/data', (req, res) => {
+//   res.json({ message: 'Hello from the backend!' });
+// });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
