e9cbac80aeee45825ada6f902d2634b32ffd08a2
Avigithub8 <sharma.avi844@gmail.com>
2024-03-17 22:55:24 +0530

    using cors

---
+++ sever.js
@@ -0,0 +1,20 @@
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(cors({
+//     origin:'http://127.0.0.1:5000', 
+// }))
+
+// app.get('/api/data', (req, res) => {
+//   res.json({ message: 'Hello from the backend!' });
+// });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
